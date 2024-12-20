35dee80f70ab5a527e72ce8335f8e57246627d1b
bishal <234aparnapaul@gmail.com>
2024-12-20 12:10:53 +0530

    Update message

--- src/components/Newsboard.jsx
+++ src/components/Newsboard.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect,useState } from 'react'
-import dotenv from 'dotenv'
 import Newsitem from './Newsitem';
 
 function Newsboard({category}) {
 
   const [articles, setArticles] = useState([]);
   useEffect(()=>{
+     const apiKey = import.meta.env.VITE_NEWS_API_KEY;
      let url1 = ` https://newsapi.org/v2/top-headlines/sources?category=businessapiKey=b1448eafb3144b1a8600dd4016ac29f0`
-     let url2 = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=b1448eafb3144b1a8600dd4016ac29f0`
+     let url2 = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`
 
       fetch(url2)
        .then(res=>res.json())
